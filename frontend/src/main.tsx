ec1db7906ef2d9556b545856ef81a832d7426c86
yujieZeng <v-yujieceng@microsoft.com>
2026-03-30 17:02:09 +0800

    update

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,3 +1,11 @@
+// Polyfill: crypto.randomUUID is unavailable in non-secure (HTTP) contexts
+if (typeof crypto !== "undefined" && typeof crypto.randomUUID !== "function") {
+  crypto.randomUUID = () =>
+    "10000000-1000-4000-8000-100000000000".replace(/[018]/g, (c) =>
+      (+c ^ (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (+c / 4)))).toString(16),
+    )
+}
+
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import { BrowserRouter } from "react-router-dom"
